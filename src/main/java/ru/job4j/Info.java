e8cecfb952efd859e7e0ed2e9e644a73142e1bdd
AlenaAgeeva <alenapvageeva@gmail.com>
2021-10-26 13:39:34 -0300

    3.7. Были  выполнены  изменения в файле Info.java на github [#243564]

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -5,6 +5,6 @@ import java.util.Calendar;
 public class Info {
     public static void main(String[] args) {
         Calendar calendar = Calendar.getInstance();
-        System.out.println(calendar.get(Calendar.DAY_OF_MONTH)+"."+calendar.get(Calendar.MONTH)+"."+calendar.get(Calendar.YEAR));
+        System.out.println(calendar.get(Calendar.DAY_OF_MONTH) + "." + calendar.get(Calendar.MONTH) + "." + calendar.get(Calendar.YEAR));
     }
 }
