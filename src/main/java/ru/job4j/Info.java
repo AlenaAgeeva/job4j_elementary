70add82601281210d57fc9f881a26e6c22be9e6c
AlenaAgeeva <alenapvageeva@gmail.com>
2021-10-27 10:14:28 -0300

    4.2. Заголовок в файле java [#159036 #220881]

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -4,7 +4,7 @@ import java.util.Calendar;
 
 public class Info {
     public static void main(String[] args) {
-        Calendar calendar = Calendar.getInstance();
-        System.out.println(calendar.get(Calendar.DAY_OF_MONTH) + "." + calendar.get(Calendar.MONTH) + "." + calendar.get(Calendar.YEAR));
+        Calendar c = Calendar.getInstance();
+        System.out.println(c.get(Calendar.DAY_OF_MONTH) + "." + c.get(Calendar.MONTH) + "." + c.get(Calendar.YEAR));
     }
 }
