526f781cf620dd6618ad1e0641f8e19f7d6321de
Alena Ageeva <alenaageeva@gmail.com>
2023-01-17 21:36:32 -0400

    5. PriorityQueue [#504944]
    Signed-off-by: Alena Ageeva <alenaageeva@gmail.com>

---
+++ src/main/java/ru/job4j/queue/ScheduleTask.java
@@ -0,0 +1,23 @@
+package ru.job4j.queue;
+
+import java.util.*;
+
+public class ScheduleTask {
+    private PriorityQueue<Task> queue;
+
+    public ScheduleTask(Comparator<Task> comparator) {
+        this.queue = new PriorityQueue<>(comparator);
+    }
+
+    public void addTask(Task task) {
+        queue.add(task);
+    }
+
+    public Task readTask() {
+        return queue.peek();
+    }
+
+    public Task getTask() {
+        return queue.poll();
+    }
+}
